9c9a2cc0150eded246aab06cd006188417fb951a
EvolveD <jrhemann@gmail.com>
2020-06-13 23:57:58 -0500

    MVP

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,7 +1,9 @@
 def linear_search(arr, target):
     # Your code here
-
-
+    for index, value in enumerate(arr):
+        if value == target:
+            return index
+            
     return -1   # not found
 
 
@@ -9,6 +11,19 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    low = 0
+    mid = len(arr)//2
+    high = len(arr)-1
 
+    while low <= high:
+        mid = (low + high) // 2
+        guess = arr[mid]
+
+        if guess == target:
+            return mid
+        elif guess < target:
+            low = mid + 1
+        elif guess > target:
+            high = mid - 1
 
     return -1  # not found
